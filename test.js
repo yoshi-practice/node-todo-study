c7ea79efd65ed03cb6ae1a058680cbbee325e7fc
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-02-23 15:34:05 +0900

    Add del test

--- test.js
+++ test.js
@@ -10,4 +10,7 @@ todo.done('ノートを買う');
 todo.done('鉛筆を買う');
 assert.deepEqual(todo.doneList(), ['ノートを買う', '鉛筆を買う']);
 
+todo.del('ノートを買う');
+console.log(todo.doneList());
+
 console.log("File loaded, OK!");
